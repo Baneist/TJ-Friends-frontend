2f43e8dd2d1097057016a2fd4fd30a100c80b838
no no <luuux@DESKTOP-BG7IJ9P>
2023-04-03 15:25:02 +0800

    show tabbar“

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -134,9 +134,9 @@ const MainScreen = () => {
       />
       <Tab.Screen
         options={{
-          tabBarStyle: {
+          /*tabBarStyle: {
             display: 'none',
-          },
+          },*/
           tabBarIcon: tabBarIcon('Trophy'),
           tabBarLabel: showLabel ? 'Trophy' : undefined,
         }}
