64813a7ce63172b8e2369cdf7870c0521b5febe1
no no <luuux@DESKTOP-BG7IJ9P>
2023-04-11 23:34:02 +0800

    update post page

--- pages/PostPage.tsx
+++ pages/PostPage.tsx
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Image } from 'react-native';
-import { Button, IconButton, Divider } from 'react-native-paper';
+import { View,  TextInput, StyleSheet, Image, Pressable } from 'react-native';
+import { Button, IconButton } from 'react-native-paper';
 import AvatarPicker from "../components/AvatarPicker/PostPicker";
+import Icon from 'react-native-vector-icons/Feather';
 
 const PostPage = () => {
   const [showAvatarOption, setShowAvatarOption] = useState(false);
   const [text, setText] = useState('');
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState([] as string[]);
 
   const handlePost = () => {
     // 发送text和image到服务器
     console.log('发布');
   };
-  function cancelAvatarOption(){
-    setShowAvatarOption(false);
+  function cancelAvatarOption() {
+    return (
+      setShowAvatarOption(false)
+    );
+  }
+  function changeImage(uri: string[]) {
+    setImage(current => current.concat(uri))
+  }
+  function out() {
+    return (
+      console.log(image)
+    );
   }
   return (
     <View>
@@ -25,21 +36,30 @@ const PostPage = () => {
           onChangeText={setText}
           multiline
         />
-        {image && (
-          <Image source={image} style={styles.image} />
-        )}
-        <IconButton
-          icon={'plus'}
-          mode='contained'
-          style={{ borderRadius: 0, margin: 10, width: 110, height: 110 }}
-          size={50}
-          onPress={()=>setShowAvatarOption(true)}
-        />
+        <View style={{ flexDirection: 'row',flexWrap:'wrap',position:'relative' }}>
+            {image.length != 0 && image.map((item,index) =>
+            <View>
+              <Image source={{ uri: item }} style={styles.image} />
+              <Pressable 
+              style={{position:'absolute',top:5,right:5, margin:0}}
+              onPress={()=>setImage(current=>current.filter((i)=>{return i!=item}))}>
+                <Icon name={'x'} style={{fontSize:15,color:'white',backgroundColor:'grey',opacity:0.6}}/>
+              </Pressable>
+              </View>
+            )}
+          {image.length <9 &&<IconButton
+            icon={'plus'}
+            mode='contained'
+            style={{ borderRadius: 0, margin: 5, width: 112, height: 112 }}
+            size={50}
+            onPress={() => setShowAvatarOption(true)}
+          />}
+        </View>
         <View style={styles.buttonContainer}>
           <Button onPress={handlePost} style={{ width: '97%' }} mode='contained'>发布</Button>
         </View>
       </View>
-      <AvatarPicker showAvatarOption={showAvatarOption} onBackdropPress={cancelAvatarOption}/>
+      <AvatarPicker showAvatarOption={showAvatarOption} onBackdropPress={cancelAvatarOption} setImage={changeImage} />
     </View>
   );
 };
@@ -56,17 +76,17 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   input: {
-    height: 100,
+    height: 140,
     borderColor: 'transparent',
     borderWidth: 1,
     padding: 10,
     fontSize: 18,
   },
   image: {
-    width: 300,
-    height: 300,
-    alignSelf: 'center',
-    marginVertical: 10,
+    borderRadius: 0,
+    margin: 5,
+    width: 112,
+    height: 112
   },
   buttonContainer: {
     flexDirection: 'row',
