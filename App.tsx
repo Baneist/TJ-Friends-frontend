01d306c8182b7f0549f3bc45f69f859c77f5f726
Vozeo Razirg <2488684796@qq.com>
2023-06-03 20:18:44 +0800

    Update folder names.

--- App.tsx
+++ App.tsx
@@ -43,11 +43,11 @@ import EditPost from "./pages/memoryManage/EditPost";
 import NoticeManageScreen from './pages/noticeManage/NoticeManage';
 import NoticeDetailScreen from "./pages/noticeManage/NoticeDetail";
 
-import ChatDetail from './pages/ChatManage/ChatDetail'
+import ChatDetail from './pages/chatManage/ChatDetail'
 import CreatePage from "./pages/roomManage/CreateRoom";
-import WaitingPage from "./pages/MatchManage/WaitingPage";
-import MatchHomePage from "./pages/MatchManage/Match";
-import CallScreen from "./pages/MatchManage/VoiceMatch";
+import WaitingPage from "./pages/matchManage/WaitingPage";
+import MatchHomePage from "./pages/matchManage/Match";
+import CallScreen from "./pages/matchManage/VoiceMatch";
 
 //房间
 import RoomInside from "./pages/roomManage/RoomInside";
